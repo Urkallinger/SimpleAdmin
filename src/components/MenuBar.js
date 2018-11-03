86e3c131ec1937d4ef487910f7cd8d6827d29324
Philipp <x@y.de>
2018-11-03 13:45:22 +0100

    added nav menu with example data

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,11 +1,16 @@
 import {
-    AppBar,
-    IconButton,
-    Toolbar,
-    Typography,
-    withStyles,
-    Menu,
-    MenuItem
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  withStyles,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+  List,
+  ListItem
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreIcon from '@material-ui/icons/MoreVert';
@@ -13,110 +18,162 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {isUndefinedOrEmpty} from '../utils/JsUtils';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const styles = {
-    root: {
-        flexGrow: 1
-    },
-    grow: {
-        flexGrow: 1
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20
-    },
-    moreButton: {
-        marginRight: -15
-    }
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  moreButton: {
+    marginRight: -15
+  }
 };
 
 const mapStateToProps = state => {
-    return {title: state.title, menuItems: state.menuItems};
+  return {title: state.title, menuItems: state.menuItems};
 };
 
 class MenuBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {anchorEl: null};
+  constructor(props) {
+    super(props);
+    this.state = {showOptMenu: false, showNavMenu: false};
+    this.optAnchor = null;
+    this.navAnchor = null;
+  }
+
+  onOpenOptMenu = () => {
+    this.setState({showOptMenu: true});
+  };
+
+  onCloseOptMenu = () => {
+    this.setState({showOptMenu: false});
+  };
+
+  onOpenNavMenu = () => {
+    this.setState({showNavMenu: true});
+  };
+
+  onCloseNavMenu = () => {
+    this.setState({showNavMenu: false});
+  };
+
+  createMenuItem = data => {
+    const onClick = () => {
+      data.onClick();
+      this.onCloseOptMenu();
+    };
+    return (
+      <MenuItem key={data.label} onClick={onClick}>
+        {data.label}
+      </MenuItem>
+    );
+  };
+
+  getOptMenu = () => {
+    const {menuItems} = this.props;
+
+    if (!isUndefinedOrEmpty(menuItems)) {
+      const {classes} = this.props;
+      const {showOptMenu} = this.state;
+      const origin = {vertical: 'top', horizontal: 'right'};
+
+      return (
+        <React.Fragment>
+          <IconButton
+            className={classes.moreButton}
+            color="inherit"
+            onClick={this.onOpenOptMenu}
+            buttonRef={node => (this.optAnchor = node)}
+          >
+            <MoreIcon />
+          </IconButton>
+          <Menu
+            anchorEl={this.optAnchor}
+            anchorOrigin={origin}
+            transformOrigin={origin}
+            open={showOptMenu}
+            onClose={this.onCloseOptMenu}
+          >
+            {menuItems.map(item => this.createMenuItem(item))}
+          </Menu>
+        </React.Fragment>
+      );
+    } else {
+      return null;
     }
+  };
 
-    onOpenOptMenu = event => {
-        this.setState({anchorEl: event.currentTarget});
-    };
-
-    onCloseOptMenu = () => {
-        this.setState({anchorEl: null});
-    };
-
-    createMenuItem = data => {
-        const onClick = () => {
-            data.onClick();
-            this.onCloseOptMenu();
-        };
-        return (
-            <MenuItem key={data.label} onClick={onClick}>
-                {data.label}
-            </MenuItem>
-        );
-    };
-
-    getOptMenu = () => {
-        const {menuItems} = this.props;
-
-        if (!isUndefinedOrEmpty(menuItems)) {
-            const {classes} = this.props;
-            const {anchorEl} = this.state;
-            const open = Boolean(anchorEl);
-            const origin = {vertical: 'top', horizontal: 'right'};
-
-            return (
-                <React.Fragment>
-                    <IconButton
-                        className={classes.moreButton}
-                        color="inherit"
-                        onClick={this.onOpenOptMenu}
-                    >
-                        <MoreIcon />
-                    </IconButton>
-                    <Menu
-                        anchorEl={anchorEl}
-                        anchorOrigin={origin}
-                        transformOrigin={origin}
-                        open={open}
-                        onClose={this.onCloseOptMenu}
-                    >
-                        {menuItems.map(item => this.createMenuItem(item))}
-                    </Menu>
-                </React.Fragment>
-            );
-        } else {
-            return null;
-        }
-    };
-
-    render() {
-        const {classes} = this.props;
-
-        return (
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton className={classes.menuButton} color="inherit">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        {this.props.title}
-                    </Typography>
-                    {this.getOptMenu()}
-                </Toolbar>
-            </AppBar>
-        );
+  getNavMenu = () => {
+    const {classes} = this.props;
+    const {showNavMenu} = this.state;
+    if (showNavMenu) {
+      return (
+        <SwipeableDrawer
+          open={this.state.showNavMenu}
+          onClose={this.onCloseNavMenu}
+          onOpen={this.onOpenNavMenu}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.onCloseNavMenu}
+            onKeyDown={this.onCloseNavMenu}
+          >
+            <div className={classes.list}>
+              <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </div>
+        </SwipeableDrawer>
+      );
+    } else {
+      return null;
     }
+  };
+
+  render() {
+    const {classes} = this.props;
+
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            buttonRef={node => (this.navAnchor = node)}
+            onClick={this.onOpenNavMenu}
+          >
+            <MenuIcon />
+          </IconButton>
+          {this.getNavMenu()}
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            {this.props.title}
+          </Typography>
+          {this.getOptMenu()}
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 MenuBar.propTypes = {
-    title: PropTypes.string,
-    menuItems: PropTypes.array,
-    classes: PropTypes.object
+  title: PropTypes.string,
+  menuItems: PropTypes.array,
+  classes: PropTypes.object
 };
 
 export default connect(mapStateToProps)(withStyles(styles)(MenuBar));
