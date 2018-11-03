5b1725f8967ba127006c9ebcb569180088a96f76
Philipp <x@y.de>
2018-11-03 13:25:24 +0100

    switched from hash history to brower history

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import {MuiThemeProvider} from '@material-ui/core';
 import {ConnectedRouter, connectRouter, routerMiddleware} from 'connected-react-router';
-import {createHashHistory} from 'history';
+import {createBrowserHistory} from 'history';
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {applyMiddleware, compose, createStore} from 'redux';
@@ -8,22 +8,22 @@ import rootReducer from './reducers/Reducers';
 import {SimpleAdminTheme} from './Theme';
 import App from './App';
 
-const history = createHashHistory();
+const history = createBrowserHistory();
 
 const store = createStore(connectRouter(history)(rootReducer),
                           compose(applyMiddleware(routerMiddleware(history))));
 window.store = store;
 
 export default class Root extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <ConnectedRouter history={history}>
-                    <MuiThemeProvider theme={SimpleAdminTheme}>
-                        <App />
-                    </MuiThemeProvider>
-                </ConnectedRouter>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <MuiThemeProvider theme={SimpleAdminTheme}>
+            <App />
+          </MuiThemeProvider>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
 }
