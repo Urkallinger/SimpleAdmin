9073b1f913828c3cbebfa879cf7d41b8f2743d17
Philipp <x@y.de>
2018-11-04 10:45:41 +0100

    codingstyle and renamed default import from routes to routing

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import MenuBar from './components/MenuBar';
 import {push} from 'connected-react-router';
-import Routes from './Routes';
+import Routing from './Routes';
 import {withStyles} from '@material-ui/core';
 
 const styles = () => ({
-    content: {
-        padding: 10
-    }
+  content: {
+    padding: 10
+  }
 });
 
 class App extends Component {
-    goToHomePage = () => {
-        window.store.dispatch(push('/home'));
-    };
+  goToHomePage = () => {
+    window.store.dispatch(push('/home'));
+  };
 
-    render() {
-        const {classes} = this.props;
-        return (
-            <div>
-                <MenuBar />
-                <div className={classes.content}>
-                    <Routes />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const {classes} = this.props;
+    return (
+      <div>
+        <MenuBar />
+        <div className={classes.content}>
+          <Routing />
+        </div>
+      </div>
+    );
+  }
 }
 
 App.propTypes = {
-    classes: PropTypes.object
+  classes: PropTypes.object
 };
 
 export default withStyles(styles)(App);
