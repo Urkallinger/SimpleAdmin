3d404a269decb19907715d58be92ad8d47ece217
Philipp <x@y.de>
2018-11-18 13:00:46 +0100

    implemented moveable fab button for example purposes only

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,18 +1,37 @@
-import {Button, withStyles} from '@material-ui/core';
+import { Button, withStyles } from '@material-ui/core';
 import MessageIcon from '@material-ui/icons/Message';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {setTitle, showMessage} from '../actions/Actions';
-import {Message} from '../model/Message';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setTitle, showMessage } from '../actions/Actions';
+import { Message } from '../model/Message';
 
-const styles = () => ({
-  msgButton: {
+const styles = theme => ({
+  msgBtn: {
     position: 'fixed',
     bottom: 10,
     right: 10
+  },
+  fabMoveUp: {
+    transform: 'translate3d(0, -55px, 0)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.easeOut
+    })
+  },
+  fabMoveDown: {
+    transform: 'translate3d(0, 0, 0)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp
+    })
   }
 });
+
+const mapStateToProps = state => {
+  return {msgShown: state.root.message ? true : false};
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -31,10 +50,12 @@ class DetailsPage extends Component {
   };
 
   render = () => {
-    const {classes} = this.props;
+    const {classes, msgShown} = this.props;
+    const fabClass = classNames(classes.msgBtn, msgShown ? classes.fabMoveUp : classes.fabMoveDown);
+
     return (
       <div>
-        <Button onClick={this.onClick} variant="fab" color="primary" className={classes.msgButton}>
+        <Button onClick={this.onClick} variant="fab" color="primary" className={fabClass}>
           <MessageIcon />
         </Button>
       </div>
@@ -45,9 +66,10 @@ class DetailsPage extends Component {
 DetailsPage.propTypes = {
   setTitle: PropTypes.func,
   showMessage: PropTypes.func,
+  msgShown: PropTypes.bool,
 
   classes: PropTypes.object.isRequired // material-ui
 };
 
-export default connect(null,
+export default connect(mapStateToProps,
                        mapDispatchToProps)(withStyles(styles)(DetailsPage));
